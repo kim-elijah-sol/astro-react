54da362ae01bfd2dd02eaf33e87c1ae0dec4182f
kim-elijah-sol <kimelijahsol@gmail.com>
2025-12-02 14:47:32 +0900

    chore: imwebme 관련 모듈 제거

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,5 @@
-import { framework }                   from '@imwebme/fe-eslint';
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 export default defineConfig([
-  framework.astro(),
   globalIgnores(['node_modules/', 'dist/', '.astro/']),
 ]);
